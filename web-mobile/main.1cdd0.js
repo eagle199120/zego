ae9efca056926d63b22e096e30a317b767e2ca90
张波 <>
2020-05-26 14:57:51 +0800

    测试6

--- web-mobile/main.1cdd0.js
+++ web-mobile/main.1cdd0.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.91df1.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.89ee3.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
