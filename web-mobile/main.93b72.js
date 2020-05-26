885acf51363eaddb9b4ca4be69d870abd9700b65
张波 <>
2020-05-26 20:35:01 +0800

    测试13

--- web-mobile/main.93b72.js
+++ web-mobile/main.93b72.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.39d14.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8bced.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
