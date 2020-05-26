28cb6bf80316df0adb3ef3ce06d7d7df76af3b05
张波 <>
2020-05-26 20:07:51 +0800

    测试12

--- web-mobile/main.d58ce.js
+++ web-mobile/main.d58ce.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.53254.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.39d14.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
