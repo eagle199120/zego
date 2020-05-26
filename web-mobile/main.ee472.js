f6501f5e3791d67e7a30787faf8714a75cd0ab20
张波 <>
2020-05-26 20:58:26 +0800

    测试16

--- web-mobile/main.ee472.js
+++ web-mobile/main.ee472.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.93f22.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e8674.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
