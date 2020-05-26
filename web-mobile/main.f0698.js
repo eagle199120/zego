ac24328760d4f0c10924ecf5a011e8543edfcb28
张波 <>
2020-05-26 17:08:44 +0800

    测试8

--- web-mobile/main.f0698.js
+++ web-mobile/main.f0698.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.44826.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.798e0.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
