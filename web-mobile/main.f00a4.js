60221de73b01da64b110e81ba621af62709f214d
张波 <>
2020-05-27 15:56:58 +0800

    测试28

--- web-mobile/main.f00a4.js
+++ web-mobile/main.f00a4.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e5310.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.c124b.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
