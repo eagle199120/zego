a1e9a2daae003c95583b955e84b7b17c42f020f1
张波 <>
2020-05-27 14:27:33 +0800

    测试27

--- web-mobile/main.e2854.js
+++ web-mobile/main.e2854.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.294f2.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e5310.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
